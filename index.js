b9c9b904e8a1ddf819bedd2b96ef2a4592aed96e
sotirelisc <gtakriz@hotmail.com>
2016-12-13 14:40:05 +0000

    Minor changes & indentation.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-  res.send('Hello world, I am a chat bot')
+  res.send('Hello botworld!')
 })
 
-// for Facebook verification
+// Facebook verification
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'tvakis_verification') {
     res.send(req.query['hub.challenge'])
@@ -27,40 +27,40 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
     }
-    res.sendStatus(200)
+  }
+  res.sendStatus(200)
 })
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  let messageData = { text:text }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
 }
 
-// Spin up the server
+// Run server
 app.listen(app.get('port'), function() {
-  console.log('running on port', app.get('port'))
+  console.log('Running on port: ', app.get('port'))
 })
