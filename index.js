336b701dfb14b4d8911933de771d52ce84c5565d
sotirelisc <gtakriz@hotmail.com>
2017-02-17 16:17:10 +0000

    Check if the movie was not found.

--- index.js
+++ index.js
@@ -80,6 +80,10 @@ app.post('/webhook/', function (req, res) {
                 console.log(err)
                 return
               }
+              if (res.results.length === 0) {
+                sendTextMessage(sender, "The TV show or movie was not found!" + not_found, null)
+                return
+              }
               sendTextMessage(sender, title_emj + " " + res.results[0].title + " (" + res.results[0].vote_average + " average)\n" + plot_emj + " " + res.results[0].overview, null)
               let release_date = new Date(res.results[0].release_date)
               sendTextMessage(sender, film + " " + res.results[0].title + " was released on " + release_date.getFullYear() + ".\n" + link + " More info at: https://www.themoviedb.org/movie/" + res.results[0].id, null)
