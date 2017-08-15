33024880a93e56f60c54ade9b96eea640a8cc7d9
sotirelisc <gtakriz@hotmail.com>
2017-08-15 16:09:50 +0300

    Rewrite searching for TV show & minor fixes.

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ let emoji = require('./assets/emoji')
 
 const poster_url = "https://image.tmdb.org/t/p/w640"
 
+bot.on('message', (payload, chat) => {
+  const fid = payload.sender.id
+  console.log(fid)
+  const text = payload.message.text.toLowerCase()
+
+  if (text == "hi" || text == "hey" || text == "hello") {
+    chat.sendTypingIndicator(500).then(() => showIntro(chat))
+  } else {
+    if (text.startsWith("movie ")) {
+      let title = getTitleFromText(text)
+      searchMovie(chat, title)
+    } else {
+      searchTv(chat, text)
+    }
+  }
+})
+
 const showIntro = (chat) => {
   chat.getUserProfile().then((user) => {
     chat.say("Hi, " + user.first_name + "!" + emoji.waving + "\n\n" +
@@ -112,7 +129,7 @@ const searchMovie = (chat, title) => {
         for (let i=0; i<movies_to_get; i++) {
           let release_date = new Date(res.results[i].release_date)
           movies.push({
-            "title": res.results[i].title + " (" + release_date + " - " + res.results[i].vote_average + "/10)",
+            "title": res.results[i].title + " (" + release_date.getFullYear() + " - " + res.results[i].vote_average + "/10)",
             "subtitle": res.results[i].overview,
             "image_url": poster_url + res.results[i].poster_path,
             "buttons": [{
@@ -129,141 +146,51 @@ const searchMovie = (chat, title) => {
 }
 
 const searchTv = (chat, title) => {
-  mdb.searchMovie({ query: title }, (err, res) => {
+  mdb.searchTv({ query: title }, (err, res) => {
     if (err) {
       console.log(err)
     } else {
       if (res.results.length === 0) {
-        chat.say("The movie was not found!" + emoji.not_found)
+        chat.say("The TV show was not found! If you're searching for a movie, add 'movie' before its title!" + emoji.not_found)
       } else {
-        let movies_to_get = res.results.length
-        // Show 5 (or less) relevant movies
-        if (movies_to_get > 5) {
-          movies_to_get = 5
-        }
-
-        let movies = []
-        for (let i=0; i<movies_to_get; i++) {
-          let release_date = new Date(res.results[i].release_date)
-          movies.push({
-            "title": res.results[i].title + " (" + release_date + " - " + res.results[i].vote_average + "/10)",
-            "subtitle": res.results[i].overview,
-            "image_url": poster_url + res.results[i].poster_path,
-            "buttons": [{
-              "type": "web_url",
-              "url": "https://www.themoviedb.org/movie/" + res.results[0].id,
-              "title": "Learn More",
-              "webview_height_ratio": "tall"
-            }]
-          })
-        }
-      }
-    }
-  })
-}
-
-bot.on('message', (payload, chat) => {
-  const fid = payload.sender.id
-  console.log(fid)
-  const text = payload.message.text.toLowerCase()
-
-  if (text == "hi" || text == "hey" || text == "hello") {
-    chat.sendTypingIndicator(500).then(() => showIntro(chat))
-  } else {
-    if (text.startsWith("movie ")) {
-      let title = getTitleFromText(text)
-      searchMovie(chat, title)
-    } else {
-      searchTv(chat, text)
-    }
-  }
-})
-
-// This is where all magic happens
-app.post('/webhook/', function (req, res) {
-  let messaging_events = req.body.entry[0].messaging
-  // Loop through messaging events
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i]
-    // Get sender (user) info
-    let sender = event.sender.id
-    // Handle regular messages
-    if (event.message && event.message.text) {
-      // Parse actual text
-      let text = event.message.text
-      // Drop messaging if the message is bigger than usual
-      if (text.length > 200) {
-        sendTextMessage(sender, "Are you sure that's the name?" + not_found, null)
-      } else {
-        // Filter text
-        text = text.toLowerCase()
-        if (text == "hi" || text == "hey" || text == "hello") {
-          sendTextMessage(sender, "Hello! Please give me a TV show or a movie (add the word 'movie' before title)!" + hug, null)
-        // Help command
-        } else if (text == "/help") {
-          sendTextMessage(sender, help_msg, null)
-        } else {
-          let title
-          // We're searching for movies
-          if (text.startsWith("movie ")) {
-            title = getTitleFromText(text)
-            mdb.searchMovie({ query: title }, (err, res) => {
-              if (err) {
-                console.log(err)
-                return
-              }
-              if (res.results.length === 0) {
-                sendTextMessage(sender, "The movie was not found!" + not_found, null)
-                return
-              }
-              sendTextMessage(sender, title_emj + " " + res.results[0].title + " (" + res.results[0].vote_average + " average)\n" + plot_emj + " " + res.results[0].overview.substring(0, 550) + "..", null)
-              let release_date = new Date(res.results[0].release_date)
-              sendTextMessage(sender, film + " " + res.results[0].title + " was released on " + release_date.getFullYear() + ".\n" + link + " More info at: https://www.themoviedb.org/movie/" + res.results[0].id, null)
-            })
-          // We're searching for TV shows
+        mdb.tvInfo({ id: res.results[0].id }, (err, res) => {
+          if (err) {
+            console.log(err)
           } else {
-            mdb.searchTv({ query: text }, (err, res) => {
+            let show = []
+            let first_aired = new Date(res.results.first_air_date)
+            show.push({
+              "title": res.results.name + " (" + release_date + " - " + res.results.vote_average + "/10)",
+              "subtitle": "First aired: " + first_aired.getFullYear(),
+              "image_url": poster_url + res.results.poster_path
+            })
+
+            // Split the overview in 2 messages if more than 550 chars (Facebook allows 640 chars message max)
+            if (res.overview.length > 550) {
+              chat.say(emoji.camera + res.overview.substring(0, 550) + "..").then(() => {
+                chat.say(".." + res.overview.substring(550, 1000))
+              })
+            } else {
+              chat.say(emoji.camera + res.overview)
+            }
+
+            // Find correct last season
+            let last_season = res.number_of_seasons
+            let last_season_date = new Date(res.seasons[last_season-1].air_date)
+            let today = new Date()
+            if (last_season_date > today) {
+              last_season--;
+            }
+            // Query last season's episodes
+            mdb.tvSeasonInfo({ id: res.id, season_number: last_season }, (err, res) => {
               if (err) {
                 console.log(err)
-                return
-              }
-              if (res.results.length === 0) {
-                sendTextMessage(sender, "The TV show was not found! If you're searching for a movie, add 'movie' before its title!" + not_found, null)
-                return
-              }
-              console.log(res.results[0].name)
-              mdb.tvInfo({ id: res.results[0].id }, (err, res) => {
-                if (err) {
-                  console.log(err)
-                  return
-                }
-                // Split the overview in 2 messages if more than 550 chars (Facebook allows 640 chars message max)
-                if (res.overview.length > 550) {
-                  sendTextMessage(sender, title_emj + " " + res.name + " (" + res.vote_average + " average)\n" + plot_emj + " " + res.overview.substring(0, 550) + "..",
-                    sendTextMessage(sender, ".." + res.overview.substring(550, 1000), null))
+              } else {
+                // If the last season has only 1 episode and later than today, then show upcoming season date
+                let temp_date = new Date(res.episodes[0].air_date)
+                if (res.episodes.length == 1 && temp_date > new Date()) {
+                  chat.say(emoji.popcorn + "New season airs on " + temp_date.toDateString() + "!")
                 } else {
-                  sendTextMessage(sender, title_emj + " " + res.name + " (" + res.vote_average + " average)\n" + plot_emj + " " + res.overview, null)
-                }
-                console.log(res.name)
-                // Find correct last season
-                let last_season = res.number_of_seasons
-                let last_season_date = new Date(res.seasons[last_season-1].air_date)
-                let today = new Date()
-                if (last_season_date > today) {
-                  last_season--;
-                }
-                // Query last season's episodes
-                mdb.tvSeasonInfo({ id: res.id, season_number: last_season }, (err, res) => {
-                  if (err) {
-                    console.log(err)
-                    return
-                  }
-                  // If the last season has only 1 episode and later than today, then show upcoming season date
-                  let temp_date = new Date(res.episodes[0].air_date)
-                  if (res.episodes.length == 1 && temp_date > new Date()) {
-                    sendTextMessage(sender, popcorn + " New season airs on " + temp_date.toDateString() + "!", null)
-                    return
-                  }
                   // Get show's last episode (if exists)
                   let last_episode = getCorrectLastEpisode(res)
                   let air_date = new Date(last_episode[0].air_date)
@@ -282,17 +209,16 @@ app.post('/webhook/', function (req, res) {
                     }
                     last_str = last_str + "\n" + view_emj + " " + next_episode.overview
                   }
-                  sendTextMessage(sender, aired_emj + last_str, null)
-                })
-              })
+                  chat.say(emoji.aired_emj + last_str)
+                }
+              }
             })
           }
-        }
+        })
       }
     }
-  }
-  res.sendStatus(200)
-})
+  })
+}
 
 // Calculates the days difference between two dates
 function getDaysDifference(date1, date2) {
